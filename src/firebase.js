67f8db627920a72bb6c3e8d47b04d103bab4c821
Ralf Vogler <ralf.vogler@gmail.com>
2019-11-13 15:54:36 +0100

    loading config in script before bundle.js also sometimes fails b/c of race

--- src/firebase.js
+++ src/firebase.js
@@ -12,18 +12,18 @@ const config = {
     appId: "1:758773997881:web:8991643725992873"
 };
 
-// firebase.initializeApp(config);
+console.log(window.firebaseConfig);
+firebase.initializeApp(window.firebaseConfig);
 
 // config can be loaded from https://firebase.google.com/docs/hosting/reserved-urls
 // works for `firebase deploy` and `firebase serve`
-console.log('A');
-(async() => {
-  const config = await (await fetch('/__/firebase/init.json')).json();
-  console.log(config);
-  // firebase is accessed in App.svelte before the above promise resolves and fails with "No Firebase App '[DEFAULT]' has been created - call Firebase App.initializeApp()""
-  firebase.initializeApp(config);
-})();
-console.log('B');
+// does not work though because there is no top-level await yet: https://github.com/tc39/proposal-top-level-await, https://v8.dev/features/top-level-await
+// (async() => {
+//   const config = await (await fetch('/__/firebase/init.json')).json();
+//   console.log(config);
+//   // firebase is accessed in App.svelte before the above promise resolves and fails with "No Firebase App '[DEFAULT]' has been created - call Firebase App.initializeApp()""
+//   firebase.initializeApp(config);
+// })();
 
 export const Auth = firebase.auth();
 export const Firestore = firebase.firestore();
