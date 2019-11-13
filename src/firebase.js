0e7e68402c4a2b805a3c410168383ae41abf2fd3
Ralf Vogler <ralf.vogler@gmail.com>
2019-11-13 15:35:12 +0100

    try init by calling async fun, but does not block

--- src/firebase.js
+++ src/firebase.js
@@ -12,16 +12,18 @@ const config = {
     appId: "1:758773997881:web:8991643725992873"
 };
 
-firebase.initializeApp(config);
+// firebase.initializeApp(config);
 
 // config can be loaded from https://firebase.google.com/docs/hosting/reserved-urls
 // works for `firebase deploy` and `firebase serve`
-fetch('/__/firebase/init.json').then(async response => {
-  const config = await response.json();
+console.log('A');
+(async() => {
+  const config = await (await fetch('/__/firebase/init.json')).json();
   console.log(config);
   // firebase is accessed in App.svelte before the above promise resolves and fails with "No Firebase App '[DEFAULT]' has been created - call Firebase App.initializeApp()""
-  // firebase.initializeApp(config);
-});
+  firebase.initializeApp(config);
+})();
+console.log('B');
 
 export const Auth = firebase.auth();
 export const Firestore = firebase.firestore();
