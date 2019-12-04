362a60b0ca517c1ce88d81d2cdfe2cb78c8fc888
Ralf Vogler <ralf.vogler@gmail.com>
2019-12-04 13:48:59 +0100

    working save in db via events for change/input

--- src/Todo.svelte
+++ src/Todo.svelte
@@ -4,16 +4,26 @@
   export let todo;
   export let delTodo;
 
-  function toggle() {
-      const ref = Firestore.doc(`todos/${todo.id}`)
-      ref.update({ done: !done })
-      console.log('toggled', todo.id)
-      // todo.done= !todo.done
+  const save = async () => {
+    console.log('save', todo)
+    const ref = await Firestore.doc(`todos/${todo.id}`).update(todo)
+    console.log('updated', ref)
   }
 
-  // want to have some generic update if data changes, but found no way to just react to local change
-  // https://svelte.dev/repl/0ebf9146c0e34aa3bb9f4ecfb783f405?version=3.15.0
-  $: console.log('change', todo) // if todo.done is changed, this runs for all todos instead of just the changed one
+  // Want to update in db whenever data changes, but found no way to just react to local change in one todo: https://svelte.dev/repl/0ebf9146c0e34aa3bb9f4ecfb783f405?version=3.15.0
+  // $: console.log('change', todo) // If e.g. todo.done is changed, this runs for all todos instead of just the changed one.
+  // One solution is to use immutable (todo example: https://svelte.dev/tutorial/svelte-options), but then the logic for toggle/save would have to be moved in the parent component with the list :(
+  // So we can't react to changes generically but need to attach functions for every change event (on:change and on:input below).
+
+  const toggle = async () => {
+    console.log('toggle', todo)
+    await save()
+  }
+
+  const textChange = async () => {
+    console.log('textChange', todo)
+    await save()
+  }
 </script>
 
 <style>
@@ -23,7 +33,7 @@
 </style>
 
 <div class:done={todo.done}>
-  <input type=checkbox bind:checked={todo.done}>
-  <input placeholder="What needs to be done?" bind:value={todo.text}>
+  <input type=checkbox bind:checked={todo.done} on:change={toggle}>
+  <input placeholder="What needs to be done?" bind:value={todo.text} on:input={textChange}>
   <button class="delete" on:click={delTodo(todo)}/>
 </div>
