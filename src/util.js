f9087f86d14cff6361434c2257c3a72b4c9d59ac
Ralf Vogler <ralf.vogler@gmail.com>
2019-12-14 21:46:17 +0100

    use const for time constants

--- src/util.js
+++ src/util.js
@@ -10,10 +10,10 @@ export const debounce = (func, delay) => {
 
 export class Duration {
   static parse(s) {
-    let m = 60
-    let h = 60*m
-    let d = 24*h
-    let y = 365*d
+    const m = 60
+    const h = 60*m
+    const d = 24*h
+    // const y = 365*d
     const f = (c,n) => s<c ? null : Math.floor(s%n/c) // c: current unit, n: next unit
     return {
       seconds: s % m,
@@ -24,8 +24,8 @@ export class Duration {
   }
 
   static str(i) {
-    let o = this.parse(i)
-    var s = ''
+    const o = this.parse(i)
+    let s = ''
     const a = (value, unit) => { if (value !== null) s+= (value < 10 ? '0' : '') + value + unit }
     a(o.days, 'd')
     a(o.hours, 'h')
